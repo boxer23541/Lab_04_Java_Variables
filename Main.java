4f56b1dc20a6bc0562b53682716a2a0e26156d0d
Boxer <alexmb2354@gmail.com>
2022-02-04 11:18:13 -0500

    Operations created

--- Main.java
+++ Main.java
@@ -2,6 +2,29 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        int intOperandA = 10;
+        int intOperandB = 5;
+
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum + ".");
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct + ".");
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference + ".");
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient + ".");
+
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The remainder of " + intOperandA + " and " + intOperandB + " is " + intModulo + ".");
+
     }
 }
